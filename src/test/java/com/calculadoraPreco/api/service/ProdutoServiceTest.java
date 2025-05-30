90f897be2def085a5f6001a6fcc53d8b68508046
ArthurNeto <arthurneto779@gmail.com>
2025-05-30 16:36:16 -0300

    melhora a cobertura dos testes do metodo updateProduto

--- src/test/java/com/calculadoraPreco/api/service/ProdutoServiceTest.java
+++ src/test/java/com/calculadoraPreco/api/service/ProdutoServiceTest.java
@@ -1,5 +1,7 @@
 package com.calculadoraPreco.api.service;
 
+import com.calculadoraPreco.api.domain.ComponenteProduto;
+import com.calculadoraPreco.api.domain.Insumo;
 import com.calculadoraPreco.api.domain.Produto;
 import com.calculadoraPreco.api.domain.Usuario;
 import com.calculadoraPreco.api.dto.ProdutoDto;
@@ -13,7 +15,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -136,7 +140,7 @@ class ProdutoServiceTest {
     }
 
     @Test
-    void update_DeveAtualizarProdutoComSucesso() {
+    void update_DeveAtualizarProdutoComSucessoComListaDeCompoenetesVazia() {
         // Arrange
         ProdutoDto produtoDto = new ProdutoDto();
         produtoDto.setId(1L);
@@ -166,6 +170,90 @@ class ProdutoServiceTest {
         verify(produtoRepository, times(1)).findById(produtoDto.getId());
         verify(produtoRepository, times(1)).save(any(Produto.class));
     }
+
+    private static Produto getProduto(BigDecimal custoUn) {
+        Insumo insumo = new Insumo();
+        insumo.setId(1L);
+        insumo.setNome("CPU");
+        insumo.setUnMedida("Unidade");
+        insumo.setCustoUn(custoUn);
+
+        ComponenteProduto componenteProduto = new ComponenteProduto();
+        componenteProduto.setId(1L);
+        componenteProduto.setQuantidade(BigDecimal.valueOf(1));
+        componenteProduto.setInsumo(insumo);
+
+        Set<ComponenteProduto> componentes = new HashSet<>();
+        componentes.add(componenteProduto);
+
+        Produto produtoExistente = new Produto();
+        produtoExistente.setId(1L);
+        produtoExistente.setNome("Notebook");
+        produtoExistente.setComponenteProduto(componentes);
+        produtoExistente.setMargemDeLucro(BigDecimal.valueOf(100));
+        return produtoExistente;
+    }
+
+    @Test
+    void update_DeveAtualizarProdutoComSucessoComListaDeCompoenetesPovoadaComCustoTotalMaiorQueZero() {
+        // Arrange
+        ProdutoDto produtoDto = new ProdutoDto();
+        produtoDto.setId(1L);
+        produtoDto.setNome("Notebook Atualizado");
+        produtoDto.setCustoTotal(BigDecimal.valueOf(100));
+        produtoDto.setPrecoVenda(new BigDecimal(200).setScale(2, RoundingMode.HALF_UP));
+        produtoDto.setMargemDeLucro(BigDecimal.valueOf(100));
+
+        Produto produtoExistente = getProduto(BigDecimal.valueOf(100));
+
+
+        when(produtoRepository.findById(produtoDto.getId())).thenReturn(Optional.of(produtoExistente));
+        when(produtoRepository.save(any(Produto.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        // Act
+        Produto resultado = produtoService.update(produtoDto);
+
+        // Assert
+        assertNotNull(resultado);
+        assertEquals(produtoDto.getId(), resultado.getId());
+        assertEquals(produtoDto.getNome(), resultado.getNome());
+        assertEquals(produtoDto.getCustoTotal(), resultado.getCustoTotal());
+        assertEquals(produtoDto.getPrecoVenda(), resultado.getPrecoVenda());
+        assertEquals(produtoDto.getMargemDeLucro(), resultado.getMargemDeLucro());
+        verify(produtoRepository, times(1)).findById(produtoDto.getId());
+        verify(produtoRepository, times(1)).save(any(Produto.class));
+    }
+
+    @Test
+    void update_DeveAtualizarProdutoComSucessoComListaDeCompoenetesPovoadaComCustoTotalMenorQueZero() {
+        // Arrange
+        ProdutoDto produtoDto = new ProdutoDto();
+        produtoDto.setId(1L);
+        produtoDto.setNome("Notebook Atualizado");
+        produtoDto.setCustoTotal(BigDecimal.valueOf(0));
+        produtoDto.setPrecoVenda(new BigDecimal(0).setScale(2, RoundingMode.HALF_UP));
+        produtoDto.setMargemDeLucro(BigDecimal.valueOf(100));
+
+        Produto produtoExistente = getProduto(BigDecimal.valueOf(-100));
+
+
+        when(produtoRepository.findById(produtoDto.getId())).thenReturn(Optional.of(produtoExistente));
+        when(produtoRepository.save(any(Produto.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        // Act
+        Produto resultado = produtoService.update(produtoDto);
+
+        // Assert
+        assertNotNull(resultado);
+        assertEquals(produtoDto.getId(), resultado.getId());
+        assertEquals(produtoDto.getNome(), resultado.getNome());
+        assertEquals(produtoDto.getCustoTotal(), resultado.getCustoTotal());
+        assertEquals(produtoDto.getPrecoVenda(), resultado.getPrecoVenda());
+        assertEquals(produtoDto.getMargemDeLucro(), resultado.getMargemDeLucro());
+        verify(produtoRepository, times(1)).findById(produtoDto.getId());
+        verify(produtoRepository, times(1)).save(any(Produto.class));
+    }
+
 
     @Test
     void update_DeveLancarExcecaoQuandoProdutoNaoExistir() {
